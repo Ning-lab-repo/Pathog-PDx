2bc151cb62fb6c30509a36892237bdba1d91b853
苏笃斌 <109675927+sudubin@users.noreply.github.com>
2026-01-20 11:10:47 +0800

    Add files via upload

--- 1 data preparation.py
+++ 1 data preparation.py
@@ -3,6 +3,8 @@ from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer,KNNImputer
+from sklearn.model_selection import train_test_split
+import joblib
 data=pd.read_csv('~.csv')
 data['Infection site'].unique()
 data_Catogry=data[['Gender(M/F)','Infection site']]
@@ -18,8 +20,14 @@ data_CatogryEnc.columns=data_Catogry.columns
 data_enc=pd.concat([data_CatogryEnc,data_Number],axis=1)
 data_enc.dropna(thresh=data_enc.shape[1]*0.65,inplace=True)
 data_enc.shape
+data_targetClass=data['Infection site']
+data_feature=data_enc.drop(columns=['Infection site'])
+X_train, X_test, y_train, y_test=train_test_split(data_feature,data_targetClass,stratify=data_targetClass,test_size=0.3,random_state=42)
 #imp=SimpleImputer(missing_values=np.nan,strategy='most_frequent')
 imp=KNNImputer(n_neighbors=300)
-data_encImpute=pd.DataFrame(imp.fit_transform(data_enc))
+X_train = imp.fit_transform(X_train)
+X_test = imp.transform(X_test)
+data_encImpute=pd.concat([pd.DataFrame(X_train), pd.DataFrame(X_test)], axis=0)
 data_encImpute.columns=data_enc.columns
 data_encImpute.to_csv('~.csv')
+joblib.dump(imp, 'knn_imputer.joblib')
